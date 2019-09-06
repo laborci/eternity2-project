0d3951ddd0662660d837b239da85e3df332bcebc
Gergely Laborci <gergely@laborci.hu>
2019-09-06 13:27:47 +0200

    no message

--- Eternity2/WebApplication/Pipeline/Middleware.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Eternity2\WebApplication\Pipeline;
-
-abstract class Middleware extends Segment {
-	final public function __invoke($method = 'run') {
-		if (method_exists($this, 'shutDown')) register_shutdown_function([$this, 'shutDown']);
-		$this->$method();
-	}
-}
--- Eternity2/WebApplication/Pipeline/Responder.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Eternity2\WebApplication\Pipeline;
-
-abstract class Responder extends Segment {
-	abstract protected function respond();
-}
