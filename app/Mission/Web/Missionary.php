85ecae1a7c2a8191cbbd8702f085075beb801a08
Gergely Laborci <gergely@laborci.hu>
2019-09-10 15:48:18 +0200

    no message

--- app/Mission/Web/Missionary.php
+++ app/Mission/Web/Missionary.php
@@ -1,12 +1,15 @@
 <?php namespace Application\Mission\Web;
 
+use Application\Mission\Web\Page\Index;
 use Eternity2\Mission\Web\Application;
 use Eternity2\Mission\Web\Routing\Router;
+use Eternity2\Thumbnail\ThumbnailResponder;
 
-class Mission extends Application {
+class Missionary extends Application {
 
 	protected function route(Router $router) {
-		$router->get('/thumbnail/*', \GhostThumbnailResponder::class)();
+		$router->get('/thumbnail/*', ThumbnailResponder::class)();
+		$router->get('/', Index::class)();
 	}
 
 }
